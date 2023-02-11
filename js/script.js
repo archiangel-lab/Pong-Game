88194fd741d4a53f4066ee65b5606355585600d5
Angelika Stano <archiangel809@gmail.com>
2023-02-11 14:23:20 +0100

    initial canvas

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.font = "30px Arial";
+
+function drawPaddle(x, y) {
+    ctx.fillRect(x, y, 20, 100);
+}
+
+function drawText(text, x, y) {
+    ctx.fillText(text, x, y);
+}
+
+function drawCircle(x, y, r) {
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.fill();
+}
+
+function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+drawPaddle(770, 100);
+drawPaddle(10, 300);
+
+drawCircle(400, 250, 15);
+
+drawText("3", 300, 50);
+drawText("6", 500, 50);
+
+
+
+
+
+
